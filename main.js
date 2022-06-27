d62038ef9c298852f702eb930bc4358a0d1c814d
alexisrizzo <alexis.rizzo21@gmail.com>
2022-06-26 21:52:45 -0300

    correccion

--- main.js
+++ main.js
@@ -1,84 +1,53 @@
 
-let correcta = 0;
 let incorrecta = 0;
 let finalizar;
 let porcentajeC;
 let porcentajeI;
 
 class Preguntas {
-    constructor (id, pregunta, respuesta,) {
+    constructor (id, pregunta, respuesta,incorrecta) {
         this.id = id
         this.pregunta = pregunta
         this.respuesta = respuesta
+        this.incorrecta = incorrecta
     }
 }
 
 arrayPreguntas = [];
-arrayPreguntas.push (new Preguntas ("a", "¿Cuál es el río más largo del mundo?"+"\n"+ "amazonas o nilo", "amazonas"))
-arrayPreguntas.push (new Preguntas ("b", "¿Cuál es el país con más habitantes del mundo?"+"\n"+ "china o rusia", "china"))
-arrayPreguntas.push (new Preguntas ("c", "¿En qué año cayó el muro de Berlín?"+"\n"+ "1990 o 1989", "1989"))
-arrayPreguntas.push (new Preguntas ("d", "¿Cuál es el planeta más grande del Sistema Solar?"+"\n"+"saturno o jupiter", "jupiter"))
-arrayPreguntas.push (new Preguntas ("e", "¿Cuántos huesos tiene el cuerpo humano?"+"\n"+"206 o 229", "206"))
-
-
-const trivia = () => {
-    const interrogante = arrayPreguntas.map ( (el) => el.pregunta)
-    const replica = arrayPreguntas.map ( (el) => el.respuesta)
-    
-    interrogante;
-    do {
-        
-        if (replica) {
-            correcta ++
-        }
-        else {
-            incorrecta ++
-        }
-        finalizar = confirm ("¿Quiere finalizar la trivia?")
-    }
-    while (finalizar) {
-            porcentajeC = correcta*100/5;
-            porcentajeI = incorrecta*100/5;
-
-            console.log ("Respuestas correctas:"+"\n"+ porcentajeC+"%")
-            console.log ("Respuestas incorrectas:"+"\n"+porcentajeI+"%")
-            if (porcentajeC>=60) {
-            console.log ("Todos pueden alcanzar el éxito pero pocos se atreven, tú lo hiciste y hoy eres uno de los mejores,"+"\n"+"¡FELICITACIONES!")
-            }
-             else (
-            console.log ("El éxito de la vida no está en vencer siempre, sino en no rendirse nunca.") )   
-    }
-
-    }
-
+arrayPreguntas.push (new Preguntas ("a", "¿Cuál es el río más largo del mundo?"+"\n"+ "amazonas o nilo", "amazonas", "nilo"))
+arrayPreguntas.push (new Preguntas ("b", "¿Cuál es el país con más habitantes del mundo?"+"\n"+ "china o rusia", "china", "rusia"))
+arrayPreguntas.push (new Preguntas ("c", "¿En qué año cayó el muro de Berlín?"+"\n"+ "1990 o 1989", "1989", "1990"))
+arrayPreguntas.push (new Preguntas ("d", "¿Cuál es el planeta más grande del Sistema Solar?"+"\n"+"saturno o jupiter", "jupiter", "saturno"))
+arrayPreguntas.push (new Preguntas ("e", "¿Cuántos huesos tiene el cuerpo humano?"+"\n"+"206 o 229", "206", "229"))
 
 
 for (const interrogacion of arrayPreguntas) {
-    let contenedor = document.createElement ("div")
-    contenedor.innerHTML = `<label for="pregunta">
-                                <p>${interrogacion.id}</p>
-                                <p>${interrogacion.pregunta}</p>
-                            </label> 
-                            <input type="text" name=${interrogacion.respuesta} id="respuestas">
+    let contenedor = document.createElement ('div')
+    contenedor.className = 'pregunta'
+    contenedor.innerHTML = `<p>${interrogacion.id}</p>
+                            <p>${interrogacion.pregunta}</p>
+                            <input type="button" value="${interrogacion.respuesta}" id= "ok">
+                            <input type="button" value="${interrogacion.incorrecta}" id= "nok">
                             `
     document.body.appendChild (contenedor)
+
+    }
+
+let verdadero = document.getElementById ("ok")
+verdadero.addEventListener ("click", resultadoOk )
+function resultadoOk () { console.log (correcta.length)
 }
-let confirmar = document.createElement ("div")
-confirmar.innerHTML = `<button id= "finalizar">finalizar</button>`
-document.body.appendChild (confirmar)
+const correcta = []
+correcta.push (resultadoOk)
+const enJSON = JSON.stringify (correcta)
 
-const enJson = JSON.stringify (arrayPreguntas);
-console.log (enJson)
+localStorage.setItem ('respuestasPositivas', enJSON)
 
-
-
-let verResultado = document.getElementById ("finalizar")
+let verResultado = localStorage.getItem ('respuestasPositivas')
 
 
-verResultado.onclick = () => {
-    trivia()
-    console.log ("ver resultado")}
 
+    
 
 
 
